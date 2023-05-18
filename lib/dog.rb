c4b867a45f0484495401b10af69dc48a785eb3bb
Mark <mark.nyambane@student.moringaschool.com>
2023-05-18 14:51:05 +0300

    Complete Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+  attr_accessor :name
+  attr_accessor :breed
+  def name=(name)
+    @name = name
+  end
 
 end
+ 
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+ 
+end
+
+fido = Dog.new
+fido.name = 'Fido'
+fido.breed = 'Shepherd'
+
+puts fido.name, fido.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-
-end
+    attr_accessor :name
+    attr_accessor :job
+    def name=(name)
+      @name = name
+    end
+  
+    def job=(job)
+      @job = job
+    end
+  
+  end
